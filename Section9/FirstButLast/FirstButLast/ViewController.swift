20a5a85853e05616d4c09687a273c39d19da889e
İlyürek Kılıç <76149506+ilyurek@users.noreply.github.com>
2023-07-03 15:15:38 +0300

    Add files via upload

---
+++ Section9/FirstButLast/FirstButLast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstButLast
+//
+//  Created by İlyürek Kılıç on 3.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
