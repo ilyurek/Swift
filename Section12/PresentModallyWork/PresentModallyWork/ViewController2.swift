824abdd3f50bb89a7855f4980a44f019c86719c0
İlyürek Kılıç <76149506+ilyurek@users.noreply.github.com>
2023-07-15 10:04:54 +0300

    Add files via upload

---
+++ Section12/PresentModallyWork/PresentModallyWork/ViewController2.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController2.swift
+//  PresentModallyWork
+//
+//  Created by İlyürek Kılıç on 8.07.2023.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func clicked(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
